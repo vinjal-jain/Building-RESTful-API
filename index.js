931bdc0a271eb7d0edd0a087bed86450d9937e18
vinjal-jain <vinjaljain14it@gmail.com>
2023-07-06 18:30:30 +0530

    Starting a Sever

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+*Primary file for the API
+*
+*/
+
+//Dependencies
+var http = require('http');
+
+//The server should respond to all requests with a string
+var server = http.createServer(function(req,res){
+    res.end('Hello World\n');
+});
+
+//Start the server, and have it listenan port 3000
+server.listen(3000,function(){
+    console.log("The server is listening on port 3000 now");
+});
