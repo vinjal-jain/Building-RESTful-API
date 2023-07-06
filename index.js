347b361ea127e6d49259b7f34ebd99722f37163a
vinjal-jain <vinjaljain14it@gmail.com>
2023-07-06 18:59:09 +0530

    adding the parsing request paths

--- index.js
+++ index.js
@@ -5,10 +5,26 @@
 
 //Dependencies
 var http = require('http');
+var url = require ('url');
+
 
 //The server should respond to all requests with a string
 var server = http.createServer(function(req,res){
+
+//Get the URl and parse it 
+var parseUrl = url.parse(req.url,true);
+
+//Get the Path 
+var path = parseUrl.pathname;
+var trimmedPath = path.replace(/^\/+$/g,'');
+
+//Send the response    
     res.end('Hello World\n');
+
+
+//Log the request path 
+console.log ('Request recieved on port ' +trimmedPath);
+
 });
 
 //Start the server, and have it listenan port 3000
